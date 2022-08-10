926a8754d6b6a058e6e70f2ff2926855858c7190
Nasiruddeen Farhan <farnas2016@gmail.com>
2022-08-10 23:45:41 +0000

    ggfggf

--- src/modules/auth/auth.controller.ts
+++ src/modules/auth/auth.controller.ts
@@ -39,7 +39,7 @@ export async function loginHandler(req: Request, res: Response) {
   res.cookie('accessToken', accessToken, {
     maxAge: 900000, // 15 mins
     httpOnly: true,
-    domain: "election1.vercel.app",
+    domain: "vercel.app",
     path: "/",
     secure: false,
   });
