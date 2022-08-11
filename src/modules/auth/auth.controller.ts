ab5628ebf116567ec161ff96ebb41d1db94897ce
Nasiruddeen Farhan <farnas2016@gmail.com>
2022-08-11 10:48:05 +0000

    hj

--- src/modules/auth/auth.controller.ts
+++ src/modules/auth/auth.controller.ts
@@ -40,7 +40,7 @@ export async function loginHandler(req: Request, res: Response) {
     maxAge: 900000, // 15 mins
     httpOnly: true,
      domain:
-      process.env.NODE_ENV !== "production" ? "localhost" : "electionapp.ga",
+      process.env.NODE_ENV !== "production" ? "localhost" : "election.umarzakari.me",
     path: "/",
     sameSite: false,
     secure: false,
